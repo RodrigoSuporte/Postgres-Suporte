c841f02bff71d5df6488106e7262d5d4313f0b6d
Matheussuporte <matheusalmeidasupor@gmail.com>
2024-06-06 11:44:02 -0300

    remoção do arquivo script.sql

--- Gestao Bloco Cirurgico/Scripts Tabelas/dadosintegraagcirurgia.sql
+++ Gestao Bloco Cirurgico/Scripts Tabelas/dadosintegraagcirurgia.sql
@@ -1,18 +1,8 @@
-CREATE TABLE percurso.agendamentodadosclinicos (
-id_agendamentodadosclinicos uuid DEFAULT gen_random_uuid() NOT NULL,
-des_condicaopaciente varchar(300) NULL,
-des_alergias varchar(600) NULL,
-id_cidprincipal uuid NOT NULL,
-num_caratercirurgia int4 NULL,
-num_reservauti int4 NULL,
-num_isolamento int4 NULL,
-num_biopsiaisolamento int4 NULL,
-num_banhoclorexidina int4 NULL ,
-num_lateralidade int4 NULL,
-des_restricao varchar(600) NULL,
-id_cirurgia uuid NOT NULL,
-CONSTRAINT agendamentodadosclinicos_pkey PRIMARY KEY (id_agendamentodadosclinicos)
+CREATE TABLE percurso.dadosintegraagcirurgia (
+	"id_dadosIntegraAgCirurgia" uuid DEFAULT gen_random_uuid() NOT NULL,
+	id_cirurgia uuid NOT NULL,
+	dadoscirurgia json NOT NULL,
+	CONSTRAINT agendamentodadosclinicos_pkey PRIMARY KEY ("id_dadosIntegraAgCirurgia")
 );
 
-ALTER TABLE percurso.agendamentodadosclinicos ADD CONSTRAINT agendamentodadosclinicos_fk1
-FOREIGN KEY (id_cirurgia) REFERENCES percurso.cirurgia(id_cirurgia);
+ALTER TABLE percurso.dadosintegraagcirurgia ADD CONSTRAINT agendamentodadosclinicos_fk1 FOREIGN KEY (id_cirurgia) REFERENCES percurso.cirurgia(id_cirurgia);
